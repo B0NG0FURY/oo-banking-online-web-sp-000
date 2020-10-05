6b6e356ee64aede1ad3fb0afd5f16021e60a2e4a
John Troutman <jtroutmanman@gmail.com>
2020-10-05 00:04:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
   
   def execute_transaction
     if @sender.balance < @amount || !@sender.valid?
-      "Transaction rejected. Please check your account balance."
+      puts "Transaction rejected. Please check your account balance."
     elsif valid? && @status == "pending" && @sender.balance > @amount
       @sender.balance -= @amount
       @receiver.deposit(@amount)
